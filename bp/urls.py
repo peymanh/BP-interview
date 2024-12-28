21ed20222984ba92fcd0bf7c7dc04d0c17f9178c
peyman.hassanabadi <peyman.hassanabadi@gandom.co>
2024-12-28 18:06:40 +0330

    urls.py: get articles service is added

--- bp/urls.py
+++ bp/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from api.views import login, signup, test_token, create_article, rate_article
+from api.views import login, signup, test_token, create_article, rate_article, ArticleViewSet
+
+router = DefaultRouter()
+router.register(r'articles', ArticleViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +30,9 @@ urlpatterns = [
     path('test-token/', test_token, name='test'),
     path('article/create/', create_article, name='create_article'),
     path('article/rate/', rate_article, name='rate_article'),
-
 ]
+
+urlpatterns += [
+    path('', include(router.urls)),
+]
+
